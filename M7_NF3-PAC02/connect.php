e114132828b96a9c4bf44bf980c72515c7d5dd37
Beatriz Vélez <145451383+VelezBeatriz@users.noreply.github.com>
2023-10-05 01:47:07 +0200

    Update connect.php

--- M7_NF3-PAC02/connect.php
+++ M7_NF3-PAC02/connect.php
@@ -18,12 +18,8 @@ $bbdd = @new mysqli(SITE_DDBB, ADMIN_USER, ADMIN_PASSWORD, DATABASE_NAME);
 
 /*
 Comandos chulis que he utilizado: 
+ - show table status;
+ - show create table people;
+ - select constraint_name, table_name, column_name from information_schema.key_column_usage where constraint_schema = 'database_name';
 */
-//select constraint_name, table_name, column_name from information_schema.key_column_usage where constraint_schema = 'moviesite';
-//show table status;
-//show create table people;
-//select m.movie_name, p.people_id from movie m inner join people p on m.movie_leadactor = p.people_id;
-
-//SELECT m.movie_name, d.people_fullname AS director, a.people_fullname AS lead_actor FROM movie m LEFT JOIN people d oN m.movie_director = d.people_id LEFT JOIN people a ON m.movie_leadactor = a.people_id;
-
 ?>
